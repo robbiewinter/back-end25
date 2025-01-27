6db7bc18b93ceb881e523d0d7801b271923c88c6
robbiewinter <robbie.winter@myy.haaga-helia.fi>
2025-01-27 05:48:21 +0200

    v3

--- Week2/Assignment2/backend/src/main/java/lists/backend/domain/Student.java
+++ Week2/Assignment2/backend/src/main/java/lists/backend/domain/Student.java
@@ -1,5 +1,27 @@
 package lists.backend.domain;
 
 public class Student {
+    String firstName;
+    String lastName;
 
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- Week2/Assignment2/backend/src/main/java/lists/backend/web/StudentController.java
+++ Week2/Assignment2/backend/src/main/java/lists/backend/web/StudentController.java
@@ -1,5 +1,27 @@
 package lists.backend.web;
 
-public class StudentController {
+import lists.backend.domain.Student;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+
+@Controller
+public class StudentController {
+    @GetMapping("/hello")
+    public String getStudent(Model model) {
+        List<Student> students = List.of(
+                new Student("Mike", "Smith"),
+                new Student("Pekka", "Lehto"),
+                new Student("Peter", "Peterson")
+        );
+
+        model.addAttribute("message", "Welcome to the Haaga-Helia");
+        model.addAttribute("students", students);
+
+        return "studentlist";
+    }
 }
